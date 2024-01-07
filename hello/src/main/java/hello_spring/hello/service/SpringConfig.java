ad8800eb39bd0e38458a3fffcb10235305f5f36e
Jung Pyo <132210134+dmsqor@users.noreply.github.com>
2024-01-07 23:10:09 +0900

    김영한 스프링 입문 6-6 스프링 데이터 JPA

--- hello/src/main/java/hello_spring/hello/service/SpringConfig.java
+++ hello/src/main/java/hello_spring/hello/service/SpringConfig.java
@@ -1,7 +1,6 @@
 package hello_spring.hello.service;
 
 import hello_spring.hello.repository.*;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,25 +15,30 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }*/
 
-
-    private EntityManager em;
-
+//JPA
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+    private final MemberRepository memberRepository;
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
-
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+/*    @Bean
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
         //return new jdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        return new JpaMemberRepository(em);//Jpa 사용시
+
+    }*/
 }
